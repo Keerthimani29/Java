5983dbaa939a66c4c29fd46d4ee6338cd9517c51
Keerthimani29 <keerthimani272@gmail.com>
2026-04-06 14:55:13 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -4,18 +4,19 @@ public class Demo{
        
         float unit=150;
         float result=0;
-        if(unit<0 && unit>100){
+        if(unit> 0 && unit<100){
             result=0;
         }
-        else if(unit < 101 && unit > 200){
+        else if(unit > 101 && unit < 200){
             result=(float)(unit*0.8f);
         }
-        else if(unit < 201 && unit > 300){
+        else if(unit > 201 && unit < 300){
             result=(float)(unit*1.5f);
         }
-        else{}
+        else{
             result=(float)(unit*3f);
         }
+
         System.out.println(result);
     }
 }
